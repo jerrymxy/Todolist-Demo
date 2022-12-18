2e2d94f4add6b9fd6dfcf216d4e0cee5b6633136
Jerrymxy <68041443+jerrymxy@users.noreply.github.com>
2022-12-18 15:56:21 +0800

    Update SQL

--- todolist.sql
+++ todolist.sql
@@ -22,7 +22,7 @@ SET time_zone = "+00:00";
 --
 
 -- 创建数据库
-CREATE DATABASE `todolist` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `todolist` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 USE todolist;
 
